29b2a71253b5ca8f3a8e94648e008ed7bcdd1ddd
Jeremy McCormick <jeremym@slac.stanford.edu>
2025-08-06 11:51:18 -0700

    Add logging utility module

---
+++ python/lsst/ppdb/gcp/log_config.py
@@ -0,0 +1,38 @@
+# This file is part of ppdb-gcp
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import logging
+import os
+
+from google.cloud.logging import Client
+
+
+def setup_logging() -> None:
+    """Set up Google Cloud Logging for the application, setting the log level
+    based on the LOG_LEVEL environment variable.
+    """
+    client = Client()
+    client.setup_logging()  # Redirects standard logging to Cloud Logging
+    log_level_str = os.getenv("LOG_LEVEL", "INFO").upper()
+    log_level = getattr(logging, log_level_str, logging.INFO)
+    logging.getLogger().setLevel(log_level)
+
+    logging.info("Logging initialized with level: %s", log_level_str)
